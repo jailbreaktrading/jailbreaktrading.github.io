658e7a049ce8ad5e9aec5185c78ce3b0f4a3ddba
jailbreaktrading <108962674+jailbreaktrading@users.noreply.github.com>
2022-07-26 20:47:38 -0400

    changed a lotta looks, added images, fixed bugs

--- app.js
+++ app.js
@@ -2,37 +2,20 @@ function Search()
 {
     var searchString=document.getElementById("search").value;
     var success = false;
-    
     var results = "";
-    /* results += ("<table>");
-    results += ("<tr>");
-    results += ("<th>Item Name</th>");
-    results += ("<th>Category</th>");
-    results += ("<th>Unobtainable</th>");
-    // results += ("<th>Sub Category</th>");
-    results += ("<th>Release Price</th>");
-    results += ("<th>Current Value</th>");
-    results += ("<th>Demand</th>");
-    // results += ("<th>Wiki Link</th>");
-    results += ("</tr>"); */
-
+ 
     for(var i = 0; i < data.length; i++)
     {
         var match = data[i].ItemName.match(new RegExp(searchString, "i"));
 
         if(match != null) 
         {
-            // results += ("<tr>");
-            // results += ("<td>" + data[i].ItemName+ "</td>");
-            results += ("<p class='jt-ItemName jt-red'><a target='_blank' href='" + data[i].JailbreakWikiLink+ "'>" + data[i].ItemName+" ("+data[i].Demand+")"+"</a></p>");
+            results += ("<a  href='" + data[i].WikiLink + "' title='Jailbreak Fandom Link (Ctrl + Click to open in new tab)'>" + "<image src=\"" + data[i].ImgLink+ "\""+ ' onerror="this.onerror=null; this.src="https://www.w3schools.com/html/pic_trulli.jpg">');
+            results += ("<p class='jt-ItemName'>" + data[i].ItemName + "</a>" + " " + "<span style='font-size:1.5rem;' class='badge bg-primary'>"+ data[i].Demand +"</span>"+"</p>");
             results += ("<p class='jt-Category jt-orange'>" + data[i].Subcategory+ " "+ data[i].Category+ "</p>");
             results += ("<p class='jt-Unobtainable jt-blue'"+ ">" + data[i].Unobtainable+ "</p>");
-            // results += ("<td>" + data[i].Subcategory+ "</td>");
             results += ("<p class='jt-ReleasePrice'>" + "Released at "+ data[i].ReleasePrice+ "</p>");
-            results += ("<p class='jt-CurrentValue jt-green'>" + "Worth "+ data[i].CurrentValue+ "</p>");
-            // results += ("<p class='jt-Demand'>" + "Demand: "+ data[i].Demand+ "</p>");
-            // results += ("<td><a target='_blank' href='" + data[i].JailbreakWikiLink+ "'>" + "Wiki Link"+"</a></td>");
-            // results += ("</tr>");
+            results += ("<p class='jt-CurrentValue jt-green'>" + "Worth "+ data[i].CurrentValue+ "</p>")
             success = true;
         }
     }
