b281d911d3356cc7ea640cfbfc6e6522db1a531d
jailbreaktrading <108962674+jailbreaktrading@users.noreply.github.com>
2022-08-17 12:29:27 -0400

    Release 1.0

--- app.js
+++ app.js
@@ -1,38 +1,31 @@
-function Search() 
-{
-    var searchString=document.getElementById("search").value;
+// JailbreakTrading v1.0 (Last updated 8/17/2022)
+function Search() {
+    var searchString = document.getElementById("search").value;
     var success = false;
     var results = "";
  
-    for(var i = 0; i < data.length; i++)
-    {
-        var match = data[i].ItemName.match(new RegExp(searchString, "i"));
-
-        if(match != null) 
-        {
-            results += ("<a  href='" + data[i].WikiLink + "' title='Jailbreak Fandom Link (Ctrl + Click to open in new tab)'>" + "<image src=\"" + data[i].ImgLink+ "\""+ ' onerror="this.onerror=null; this.src="https://www.w3schools.com/html/pic_trulli.jpg">');
-            results += ("<p class='jt-ItemName'>" + data[i].ItemName + "</a>" + " " + "<span style='font-size:1.5rem;' class='badge bg-primary'>"+ data[i].Demand +"</span>"+"</p>");
-            results += ("<p class='jt-Category jt-orange'>" + data[i].Subcategory+ " "+ data[i].Category+ "</p>");
-            results += ("<p class='jt-Unobtainable jt-blue'"+ ">" + data[i].Unobtainable+ "</p>");
-            results += ("<p class='jt-ReleasePrice'>" + "Released at "+ data[i].ReleasePrice+ "</p>");
-            results += ("<p class='jt-CurrentValue jt-green'>" + "Worth "+ data[i].CurrentValue+ "</p>")
-            success = true;
-        }
+    for (var i = 0; i < data.length; i++) {
+       var match = data[i].ItemName.match(new RegExp(searchString, "i"));
+ 
+       if (match != null) {
+          results += ("<a  href='" + data[i].WikiLink + "' title='Jailbreak Fandom Link (Ctrl + Click to open in new tab)'>" + "<image src=\"" + data[i].ImgLink + "\"" + "alt='" + data[i].ItemName + "'" + '>');
+          results += ("<p class='jt-ItemName'><b>" + data[i].ItemName + "</a>" + " " + "<span style='font-size:1.5rem;' class='badge bg-primary'>" + data[i].Demand + "</span>" + "</b></p>");
+          results += ("<p class='jt-Category jt-orange'>" + data[i].Subcategory + " " + data[i].Category + "</p>");
+          results += ("<p class='jt-Unobtainable jt-blue'" + ">" + data[i].Unobtainable + "</p>");
+          results += ("<p class='jt-ReleasePrice'>" + "Released at " + data[i].ReleasePrice + "</p>");
+          results += ("<p class='jt-CurrentValue jt-green'>" + "Worth " + data[i].CurrentValue + "</p>")
+          success = true;
+       }
     }
-
-    results += ("</table>");
-
-    if(success) 
-    {
-        document.getElementById("SearchDisplay").style.display = 'block';
-        document.getElementById("NoMatchDisplay").style.display = 'none';
-        document.getElementById("SearchDisplay").innerHTML = results;
+ 
+    if (success) {
+       document.getElementById("SearchDisplay").style.display = 'block';
+       document.getElementById("NoMatchDisplay").style.display = 'none';
+       document.getElementById("SearchDisplay").innerHTML = results;
+    } else {
+       document.getElementById("NoMatchDisplay").style.display = 'block';
+       document.getElementById("SearchDisplay").style.display = 'none';
     }
-    else
-    {
-        document.getElementById("NoMatchDisplay").style.display = 'block';
-        document.getElementById("SearchDisplay").style.display = 'none';
-    }
-
+ 
     return false;
-}
+ }
